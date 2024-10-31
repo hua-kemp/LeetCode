8e3ebd5228c4968f1371f8a8ffc9e73bfa520f5e
Josh Kempson <joshuakempson99@gmail.com>
2024-10-31 12:30:19 +0000

    Update

--- 14. Longest Common Prefix.py
+++ 14. Longest Common Prefix.py
@@ -1,19 +1,27 @@
 def longestCommonPrefix(strs: list[str]) -> str:
-    max = 0
-    longest = ""
+#    max = 0
+    ans = ""
 
-    for x in strs:
-        if len(x) > max:
-            max = len(x)
-    
-    for y in range(max):
-        for z in range(y+1, max):
-            if strs[0][y:z] == strs[1][y:z] == strs[2][y:z] and z - y > len(longest):
-                longest = strs[0][y:z]
+    sort_strs = sorted(strs)
+    first = sort_strs[0]
+    last = sort_strs[-1]
 
-    return(longest)
+#    for x in strs:
+#        if len(x) > max:
+#            max = len(x)
+#    
+#    for y in range(max):
+#        for z in range(y+1, max):
+#            if strs[0][y:z] == strs[1][y:z] == strs[2][y:z] and z - y > len(ans):
+#                ans = strs[0][y:z]
+
+    for x in range(min(len(strs), len(last))):
+        if(first[x] != last[x]):
+            return ans
+        ans += first[x]
+    return ans
             
 
 
-longestCommonPrefix(["flower","flow","flight"])
-longestCommonPrefix(["dog","racecar","car"])
+print(longestCommonPrefix(["flower","flow","flight"]))
+print(longestCommonPrefix(["dog","racecar","car"]))
